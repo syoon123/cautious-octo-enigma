e7acb87e3091b7eb5d5f51af15a1314d9bf9dca6
Jerry Chang <jerrychang22@gmail.com>
2017-02-07 22:45:17 -0500

    Update db_builder.py

--- db_builder.py
+++ db_builder.py
@@ -6,24 +6,28 @@ db = server.coctoenigma
 students = db.students
 courses = db.courses
 
-f1 = open("peeps.csv")
-f2 = open("courses.csv")
-d1 = csv.DictReader(f1)
-d2 = csv.DictReader(f2)
+d1 = csv.DictReader(open("peeps.csv"))
+d2 = csv.DictReader(open("courses.csv"))
 
-def getStudents(dict):
-    for k in dict:
-        students.insert_one(k)
-        print k
-    return 
-
-def getCourses(dict):
-    for k in dict:
-        courses.insert_one(k)
-        print k
+def getStudents(peeps, courses):
+    studentList = {}
+    for k in peeps:
+        studentList[k["id"]] = {
+            "name": k["name"],
+            "age": k["age"],
+            "id": k["id"],
+            "courses": []
+        }
+    for k in courses:
+        if k["id"] in studentList:
+            studentList[k["id"]]["courses"].append({
+                "code": k["code"],
+                "mark": k["mark"],
+            })
+    
+    db.students.insert_many(studentList.values())
     return
 
-getStudents(d1)
-getCourses(d2)
+getStudents(d1, d2)
 print students.count()
 print courses.count()
